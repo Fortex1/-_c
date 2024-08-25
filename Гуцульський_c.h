3ae7f67969db9297c392e3aa8af7ec5d0694e053
Fortex1 <mykolademan@gmail.com>
2024-08-25 09:52:19 +0300

    Add files via upload

---
+++ Гуцульський_c.h
@@ -0,0 +1,23 @@
+#ifndef ГУЦУЛЬСЬКИЙ_C_H
+#define ГУЦУЛЬСЬКИЙ_C_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ґазда int main
+#define цифра int
+#define буква char
+#define гейзлик void
+#define чмалювати printf
+#define запитати scanf
+#define якшо if
+#define інакше else if 
+#define ніґде else
+#define для for
+#define ріпу_копаю while
+#define дати malloc
+#define забрати free
+#define хата struct 
+
+
+#endif //ГУЦУЛЬСЬКИЙ_C_H
